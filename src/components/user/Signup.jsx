de0afa81822be63ec1ca1f7e42a4edb46319d00a
Nasif Swalah <nasifswalah@gmail.com>
2024-05-10 12:02:56 +0530

    setup the user signup

--- src/components/user/Signup.jsx
+++ src/components/user/Signup.jsx
@@ -1,22 +1,64 @@
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import axios from "axios";
+
+const schema = yup.object({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  email: yup.string().required().email(),
+  password: yup.string().required().min(6),
+});
 
 const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
 
-    const {register, handleSubmit}= useForm()  
-    
-    const onSubmit = (data) => console.log(data)
-    
-    return (
-        <div className="flex h-screen justify-center items-center" >
-            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-5 p-10 shadow-lg shadow-slate-300  rounded-md " >
-                <input {...register("firstName")} placeholder="First Name" className="border rounded-md text-gray-100 px-1.5 py-2 " />
-                <input {...register("lastName")} placeholder="Last Name"  className="border rounded-md text-gray-100 px-1.5 py-2 " />
-                <input {...register("email")} placeholder="Email"  className="border  rounded-md text-gray-100 px-1.5 py-2 " />
-                <input {...register("password")} placeholder="Password"  className="border rounded-md text-gray-100 px-1.5 py-2 " />
-                <input type="submit" className="rounded-md text-white bg-blue-500 hover:bg-blue-400 py-1 mt-4" />
-            </form>
-        </div>
-    )
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3000/api/v1/user/signup", data);
+  };
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-y-5 rounded-md p-10 shadow-lg  shadow-slate-300 "
+      >
+        <input
+          {...register("firstName")}
+          placeholder="First Name"
+          className="rounded-md border px-1.5 py-2 text-gray-100 "
+        />
+        {errors.firstName && <p className="text-red-500" >{errors.firstName.message}</p>}
+        <input
+          {...register("lastName")}
+          placeholder="Last Name"
+          className="rounded-md border px-1.5 py-2 text-gray-100 "
+        />
+        {errors.lastName && <p className="text-red-500" >{errors.lastName.message}</p>}
+        <input
+          {...register("email")}
+          placeholder="Email"
+          className="rounded-md  border px-1.5 py-2 text-gray-100 "
+        />
+        {errors.email && <p className="text-red-500" >{errors.email.message}</p>}
+        <input
+          {...register("password")}
+          type="password"
+          placeholder="Password"
+          className="rounded-md border px-1.5 py-2 text-gray-100 "
+        />
+        {errors.password && <p className="text-red-500" >{errors.password.message}</p>}
+        <input
+          type="submit"
+          className="mt-4 rounded-md bg-blue-500 py-1 text-white hover:bg-blue-400"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
